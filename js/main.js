80e7630f83072b80210716b0a831012ed37f0fe8
ArantzaGG <arantzagomez@hotmail.es>
2023-09-19 16:30:22 +0200

    ejercicio resuelto

--- js/main.js
+++ js/main.js
@@ -1,59 +1,92 @@
 'use strict';
 // queryselector
+
+let playerScore = 0;
+let pcScore = 0;
+let moves = 0;
+
 const select = document.querySelector('.js-select');
-const stone = document.querySelector('.js-stone');
-const paper = document.querySelector('.js-paper');
-const scissors = document.querySelector('.js-scissors');
 const btnPlay = document.querySelector('.js-btn-play');
 const letsPlay = document.querySelector('.js-lets-play');
 const counterPlayer = document.querySelector('.js-counter-player');
 const counterPc = document.querySelector('.js-counter-pc');
-const result = document.querySelector('js-result');
+const result = document.querySelector('.js-result');
+const btnReset = document.querySelector('.js-btn-hidden');
 
 // funciones
 
 function getRandomNumber(max) {
-  const randomNumber = Math.ceil(Math.random() * max);
-  return randomNumber;
+  return Math.ceil(Math.random() * max);
 }
 
 function computerPlay() {
   const randomNumber = getRandomNumber(9);
+  let pcValue = '';
   if (randomNumber <= 3) {
-    letsPlay.innerHTML = 'Piedra';
-    //letsPlay.classList.add("stone");
+    pcValue = 'piedra';
+  } else if (randomNumber >= 7) {
+    pcValue = 'papel';
+  } else {
+    pcValue = 'tijera';
   }
-  if (randomNumber > 3) {
-    letsPlay.innerHTML = 'Papel';
-    //letsPlay.classList.add("paper");
+  return pcValue;
+}
+function paintScore() {
+  counterPlayer.innerHTML = playerScore;
+  counterPc.innerHTML = pcScore;
+}
+function playGame() {
+  const valuePc = computerPlay();
+  const valueUser = select.value;
+
+  console.log(valuePc);
+  console.log(valueUser);
+  if (valueUser !== '') {
+    moves++;
+    if (valuePc === valueUser) {
+      result.innerHTML = '¡EMPATE!';
+    } else if (valueUser === 'piedra') {
+      if (valuePc === 'tijera') {
+        result.innerHTML = '¡TÚ GANAS!';
+        playerScore++;
+      } else {
+        result.innerHTML = '¡TÚ PIERDES!';
+        pcScore++;
+      }
+    } else if (valueUser === 'papel') {
+      if (valuePc === 'tijera') {
+        result.innerHTML = '¡TÚ PIERDES!';
+        pcScore++;
+      } else {
+        result.innerHTML = '¡TÚ GANAS!';
+        playerScore++;
+      }
+    } else if (valueUser === 'tijera') {
+      if (valuePc === 'papel') {
+        result.innerHTML = '¡TÚ PIERDES!';
+        pcScore++;
+      } else {
+        result.innerHTML = '¡TÚ GANAS!';
+        playerScore++;
+      }
+    }
   }
-  if (randomNumber > 7) {
-    letsPlay.innerHTML = 'Tijera';
-    //letsPlay.classList.add("scissors");
+  paintScore();
+}
+
+function gameOver() {
+  if (moves >= 10) {
+    btnReset.classList.remove('hidden');
+    btnPlay.classList.add('hidden');
   }
 }
 
-// function comparation() {
-
-//   if () {
-//     result.innerHTML = '¡EMPATE!';
-//   }
-
-//   if () {
-//      result.innerHTML = '¡TÚ GANAS!';
-//    }
-//    if () {
-//      result.innerHTML = '¡TÚ PIERDES!';
-//    }
-//}
-
 function handleClick(event) {
   event.preventDefault();
-  console.log(getRandomNumber(9));
-  //console.log(comparation());
-  getRandomNumber(9);
-  computerPlay();
-  //comparation();
+  console.log(computerPlay());
+  console.log(select.value);
+  playGame();
+  gameOver();
 }
 
 // manejadoras
